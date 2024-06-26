aaabe3e3287572c28511797b6057bd92e5148875
SunKyeongJung <tjsrud0314@gmail.com>
2024-06-27 00:59:39 +0900

    섹션7-2

--- src/main/java/study/demo/member/MemoryMemberRepository.java
+++ src/main/java/study/demo/member/MemoryMemberRepository.java
@@ -1,5 +1,6 @@
 package study.demo.member;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import study.demo.member.entity.Member;
 
@@ -13,6 +14,7 @@ import java.util.Map;
  */
 
 @Component  // memoryMemberRepository
+@Primary
 public class MemoryMemberRepository implements MemberRepository {
 
     /**

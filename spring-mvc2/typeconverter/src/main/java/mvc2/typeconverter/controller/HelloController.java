012784c7d99487a0dc9caf439b9148b6aa32ad2c
SunKyeongJung <tjsrud0314@gmail.com>
2024-09-07 01:22:15 +0900

    스프링MVC2 - 섹션 10-2

--- spring-mvc2/typeconverter/src/main/java/mvc2/typeconverter/controller/HelloController.java
+++ spring-mvc2/typeconverter/src/main/java/mvc2/typeconverter/controller/HelloController.java
@@ -2,6 +2,7 @@ package mvc2.typeconverter.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import mvc2.typeconverter.type.IpPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,17 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello-v1")
-    public String helloV1(HttpServletRequest request) {
-        String data = request.getParameter("data");
-        Integer intData = Integer.valueOf(data);
-        log.info("helloV1 intData: {}", intData);
-        return "ok";
-    }
+	@GetMapping("/hello-v1")
+	public String helloV1(HttpServletRequest request) {
+		String data = request.getParameter("data");
+		Integer intData = Integer.valueOf(data);
+		log.info("helloV1 intData: {}", intData);
+		return "ok";
+	}
 
-    @GetMapping("/hello-v2")
-    public String helloV2(@RequestParam Integer data) {
-        log.info("helloV2 data: {}", data);
-        return "ok";
-    }
+	@GetMapping("/hello-v2")
+	public String helloV2(@RequestParam("data") Integer data) {
+		log.info("helloV2 data: {}", data);
+		return "ok";
+	}
+
+	@GetMapping("/ip-port")
+	public String ipPort(@RequestParam("ipPort") IpPort ipPort) {
+		System.out.println("ipPort.getIp() = " + ipPort.getIp());
+		System.out.println("ipPort.getPort() = " + ipPort.getPort());
+		return "ok";
+	}
 }

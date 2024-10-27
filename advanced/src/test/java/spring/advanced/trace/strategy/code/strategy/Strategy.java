eca4c75b8c53e8dd7eef16aecb321ca2592a0eb8
SunKyeongJung <tjsrud0314@gmail.com>
2024-10-27 23:06:11 +0900

    경로 이동

--- advanced/src/test/java/spring/advanced/trace/strategy/code/strategy/Strategy.java
+++
@@ -1,5 +0,0 @@
-package spring.advanced.trace.strategy.code.strategy;
-
-public interface Strategy {
-	void call();
-}
--- advanced/src/test/java/spring/advanced/trace/strategy/code/template/Callback.java
+++
@@ -1,6 +0,0 @@
-package spring.advanced.trace.strategy.code.template;
-
-public interface Callback {
-
-	void call();
-}

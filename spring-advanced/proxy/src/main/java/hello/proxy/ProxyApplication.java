e9b96c8301e0ea7f14c8f4319a98e8c60c34245d
SunKyeongJung <tjsrud0314@gmail.com>
2024-10-28 23:18:16 +0900

    스프링 핵심원리 고급 5-3

--- spring-advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ spring-advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,10 +1,18 @@
 package hello.proxy;
 
+import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
+import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
 //@Import(AppV1Config.class)
 //@Import(AppV2Config.class)
+//@Import(InterfaceProxyConfig.class)
+@Import(ConcreteProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app.v3")
 public class ProxyApplication {
 
@@ -12,4 +20,8 @@ public class ProxyApplication {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
 
+	@Bean
+	public LogTrace trace() {
+		return new ThreadLocalLogTrace();
+	}
 }
